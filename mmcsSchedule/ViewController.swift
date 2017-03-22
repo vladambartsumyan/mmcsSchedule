7fd4ccfba348f6300bd30e1596983a795c08e587
Vlad Ambartsumyan <ambarvlad@gmail.com>
2017-03-22 20:28:46 +0300

    Created empty project.

---
+++ mmcsSchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mmcsSchedule
+//
+//  Created by Владислав Амбарцумян on 22.03.17.
+//  Copyright © 2017 Владислав Амбарцумян. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
